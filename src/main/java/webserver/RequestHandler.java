720d8c75df35cd3f150110b6dd3c9df582a20cd2
Do-oya <qkrwjdgus1441@naver.com>
2024-09-24 14:05:10 +0900

    feat: BufferedReader.readLine()을 활용하여 HTTP 요청을 라인별로 읽는 기능 구현
    - BufferedReader.readLine()을 사용해 HTTP 요청 정보를 라인별로 읽을 수 있도록 구현.
    - HTTP 요청 정보를 출력하는 부분에서 어려움이 있었으나, 힌트를 참고하여 해결.

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,9 +1,6 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
 
 import org.slf4j.Logger;
@@ -22,7 +19,20 @@ public class RequestHandler extends Thread {
         log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
 
+        // TODO 요구사향 1 - index.html 응답하기
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            String line = br.readLine();
+
+            if (line == null) {
+                return;
+            }
+
+            while (!"".equals(line)) {
+                log.debug("Received request: {}", line);
+                line = br.readLine();
+            }
+
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
             byte[] body = "Hello World".getBytes();
